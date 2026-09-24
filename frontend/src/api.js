395a052c6117d0a2b4f96c1c35d844164087e229
SrriHariTR <trsrrihari@gmail.com>
2026-09-24 10:25:40 +0530

    Delete frontend directory

--- frontend/src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API_URL = "http://localhost:7071/api";
-
-export const getTelemetry = async () => {
-  const response = await axios.get(`${API_URL}/telemetry`);
-  return response.data;
-};
